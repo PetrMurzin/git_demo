b3437054b3c57a663ea441da30310f8cd0cbfe0f
PetrMurzin <petr.t.murzin@gmail.com>
2023-05-10 15:11:01 +0300

    add

--- import_selenium.py
+++ import_selenium.py
@@ -5,3 +5,4 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
